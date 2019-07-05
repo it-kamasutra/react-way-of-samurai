7a02d2fece7a0a990d03b1ba369949a2a720c02d
kuzyuberdin <kuzyuberdin@gmail.com>
2019-07-06 01:02:49 +0300

    74: componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -25,7 +25,19 @@ class ProfileStatus extends React.Component {
         });
     }
 
+    componentDidUpdate(prevProps, prevState) {
+
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+        }
+
+        console.log("componentDidUpdate")
+    }
+
     render() {
+        console.log("render")
         return (
             <div>
                 {!this.state.editMode &&
