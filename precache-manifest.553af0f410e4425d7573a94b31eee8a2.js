1e7a3e254254e1d607dc41317adee10d396e9db8
kuzyuberdin <kuzyuberdin@gmail.com>
2019-07-23 23:20:21 +0300

    Updates

---
+++ precache-manifest.553af0f410e4425d7573a94b31eee8a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e22e8bdffe4f3cd21923c5f5928c07",
+    "url": "/test-bla-repo/index.html"
+  },
+  {
+    "revision": "cc5c1c8afb7cd375f6af",
+    "url": "/test-bla-repo/static/css/main.f71074d5.chunk.css"
+  },
+  {
+    "revision": "8433ff5e85656187b8f4",
+    "url": "/test-bla-repo/static/js/2.8d4a10f1.chunk.js"
+  },
+  {
+    "revision": "cc5c1c8afb7cd375f6af",
+    "url": "/test-bla-repo/static/js/main.2562d732.chunk.js"
+  },
+  {
+    "revision": "a4786d2c5f8c312f3388",
+    "url": "/test-bla-repo/static/js/runtime~main.1ef74007.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/test-bla-repo/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/test-bla-repo/static/media/user.8b1dbb1f.png"
+  }
+]);
