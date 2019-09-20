ce117340c25839b5938b6d9c0ee4196be51fadf5
Dimych <kuzyuberdin@gmail.com>
2019-09-20 20:17:22 +0300

    BrowserRouter

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,8 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers:     {
-        "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
+        "API-KEY": "8cb29b96-1ff9-457a-9229-34cee0202934"
+        //"API-KEY": "4c2a720a-b6c7-4460-83f3-bfe52edab881"
     }
 });
 
