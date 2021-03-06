0550136c427a4c6b32dc3a21eca2a12020647f4f
Gorb1k <73354019+Gorb1k@users.noreply.github.com>
2021-03-06 12:18:41 +0200

    1 changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Technologies from './Technologies';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Technologies />
+      <Footer />
+    </div>);
+}
+
+export default App;
